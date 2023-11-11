c24249c2730c6334e374b8815c748ddfb51af379
AlbertoMessineo <albevorox@gmail.com>
2023-11-11 18:01:53 +0100

    feat: js inizialized

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+/* <div class="slide ">
+<img src="img/01.jpg" alt="img-1">
+</div>
+<div class="slide">
+<img src="img/02.jpg" alt="">
+</div>
+<div class="slide active">
+<img src="img/03.jpg" alt="">
+</div>
+<div class="slide">
+<img src="img/04.jpg" alt="">
+</div>
+<div class="slide">
+<img src="img/05.jpg" alt="">
+</div> */
+ const images = ['01.jpg' , '02.jpg', '03.jpg' ,'04.jpg' , '05.jpg' ];
+ const slider = document.querySelector('.slider');
+
+ for( let i = 0; i < images.length; i++){
+    
+    //slide
+    const slide= document.createElement('div');
+    slide.classList.add('slide');
+ }
